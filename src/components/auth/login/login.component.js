638f83792115837b14a41c67c53b01c88b235c89
nilaw2017 <nilaw.manandhar2016@gmail.com>
2021-11-11 22:05:17 +0545

    Static Login page

--- src/components/auth/login/login.component.js
+++ src/components/auth/login/login.component.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
 class Login extends React.Component {
+    state = {
+        username: "Nilaw"
+    }
+
     render() { 
+        console.log("state ===>", this.state);
+
         return (
             <React.Fragment>
-                <h1>Login</h1>
                 
+                <form className="border container p-5 form-group mt-5">
+                    <h1>Login</h1>
+                    <label htmlFor="username"></label>
+                    <input className="form-control" type="text" placeholder="Username" name="username" id="username"></input>
+
+                    <label htmlFor="password"></label>
+                    <input className="form-control" type="password" placeholder="Password" name="password" id="password"></input>
+
+                    <hr></hr>
+
+                    <button className="btn bg-success" type="submit">Login</button>
+                </form>
             </React.Fragment>
         );
     }
